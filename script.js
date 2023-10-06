b34d20814917d72d1b722e7166bb7ba649889da8
habibgit001 <146697808+habibgit001@users.noreply.github.com>
2023-10-06 20:08:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const slides = document.querySelectorAll(".slide")
+
+var counter = 0;
+slides.forEach(
+    (slide, index)=>{
+        slide.style.top = `${index*100}%`
+
+    }
+)
+
+const goPrev = ()=>{
+    counter--
+    slideImage()
+}
+
+const goNext = ()=>{
+    counter++
+    slideImage()
+}
+
+const slideImage = ()=>{
+    slides.forEach(
+        (slide)=>{
+            slide.style.transform = `translateY(-${counter*100}%)`
+        }
+    )
+}
